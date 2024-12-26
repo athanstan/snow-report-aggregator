8cac73d485878ef5cfa0378f2c81e49789f00925
d3adr1nger <d3adr1nger@hackthebox.eu>
2024-12-26 20:25:45 +0200

    Add slope counts which i forgot :P

--- app/Http/Controllers/Api/MainDataController.php
+++ app/Http/Controllers/Api/MainDataController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers\Api;
 
+use App\Enums\SnowReportLiftStatus;
 use App\Http\Controllers\Controller;
 use App\Models\SnowReport;
 use Illuminate\Http\Request;
@@ -12,7 +13,10 @@ class MainDataController extends Controller
     public function index(Request $request)
     {
         $SnowReports = Cache::remember('snow_reports', 60 * 60, function () {
-            return SnowReport::all();
+            return SnowReport::withCount([
+                'slopes as total_slopes_count',
+                'slopes as open_slopes_count' => fn($query) => $query->where('status', SnowReportLiftStatus::OPEN->toStatusString()),
+            ])->get();
         });
 
         return response()->json($SnowReports);
@@ -21,7 +25,10 @@ class MainDataController extends Controller
     public function show(Request $request, int $id)
     {
         $SnowReport = Cache::remember('snow_report_' . $id, 60 * 60, function () use ($id) {
-            return SnowReport::findOrFail($id);
+            return SnowReport::withCount([
+                'slopes as total_slopes_count',
+                'slopes as open_slopes_count' => fn($query) => $query->where('status', SnowReportLiftStatus::OPEN->toStatusString()),
+            ])->findOrFail($id);
         });
 
         return response()->json($SnowReport);
